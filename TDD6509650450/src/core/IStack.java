153b0beb63e6b753fd13ab5d950c76ff6b878af9
Tanchanok6509650450 <144791909+Tanchanok6509650450@users.noreply.github.com>
2023-11-24 20:02:05 +0700

    Add files via upload

---
+++ TDD6509650450/src/core/IStack.java
@@ -0,0 +1,11 @@
+package core;
+
+public interface IStack {
+	int getSize() ;
+	boolean isEmpty() ;
+	boolean isFull();
+	void push(Object ele) throws Exception;
+	Object pop();
+	Object top();
+
+}
